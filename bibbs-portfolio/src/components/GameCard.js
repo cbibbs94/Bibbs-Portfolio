2dac848f5aecac0d0a56f2b9dd912f5acc3191b8
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-11-15 17:37:41 -0800

    more styling, set-up carousel, adjusted sizes for layout

--- bibbs-portfolio/src/components/GameCard.js
+++ bibbs-portfolio/src/components/GameCard.js
@@ -1,22 +1,37 @@
-import Card from 'react-bootstrap/Card';
+
 import gameImageOne from '../images/game1.PNG';
 import gameImageTwo from '../images/game2.PNG';
+import Image from 'react-bootstrap/Image';
+import Carousel from 'react-bootstrap/Carousel';
 
 
-const GameCard = () => {
+function GameCard() {
     return (
-        <Card style ={{width: "18rem"}}>
-            <Card.Img variant='top' src={gameImageOne} height={350} width={550}  />
-            <Card.Body>
-                <Card.Title> Sword Logic: Survival of The Mighty</Card.Title>
-                <Card.Text>
-                    This Javascript and HTML based RPG game dives head first into what Obeject Oriented and Class Based programming can really do when correctly implemented
+      <Carousel>
+        {/* First Picture */}
+        <Carousel.Item>
+          <img className='d-block w-100' src={gameImageOne} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        {/* Second Picture */}
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={gameImageTwo}
+            alt="Second slide"
+            
+          />
 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fringilla bibendum est. Vivamus nec finibus purus. Mauris porttitor imperdiet faucibus. Integer semper dolor nunc, nec pellentesque risus semper vel. Curabitur tincidunt pulvinar bibendum. In sed ante lacinia, vestibulum sapien ut, venenatis sem. Morbi nec iaculis sem. Praesent quis tincidunt orci.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    )
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
 } 
 
 
